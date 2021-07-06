9a845d798245dd9c24f5846899a8630c3138e039
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-07-06 15:25:21 -0500

    feat: added axios to get data from unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
+import axios from "axios";
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return (
-    <div className="ui container" style={{ marginTop: '10px'}}>
-        <SearchBar/>
-    </div>
-    );
+class App extends React.Component {
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID bY8ZBkPEfamEECgS589dFoxCEB7usxYkqQcc0AB98-M'
+            }
+        });
+    }
+
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
 }
 
 export default App;
